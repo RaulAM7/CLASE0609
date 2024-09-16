24efd88f579abf4601fca949516c2cc1b1ca0426
RaulAM <raulartilesm@gmail.com>
2024-09-16 16:43:01 +0100

    Update: trabajo sobre recursividad. Compuesto por notas teoricas y ejercicios de practica.

--- clase0609.js
+++ clase0609.js
@@ -22,3 +22,42 @@ console.log(myName) // Esta linea no llama a la funcion por lo que el valor de l
 
 
 // Ejemplos de funciones varias
+
+// Problema: diseñar una funcion que pille un numero como parametro y lo eleve al cuadrado
+
+function squareIt (a) {
+
+    let result = a**2
+
+    return result
+}
+
+
+/*
+    Problema: diseñar una funcion PURA que de una lista de los primeros N cuadrados  
+            -> NO SE PUEDE PORQUE ES PURA Y RECURSIVA, POR LO QUE SI QUEREMOS MODIFICAR UN ARRAY SIEMPRE LO VAMOS A SOBREESCRIBIR EN CADA ITERACION 
+
+*/
+
+function squareFirstN (a, n) {
+    
+    const listNSquares = []
+
+        
+    if (a > n) {
+    
+        return
+        
+    } else {
+    
+        console.log(a)
+        listNSquares.push(squareIt(a))
+        console.log(listNSquares)
+             
+    } 
+
+    return squareFirstN(a + 1, n)        
+} 
+ 
+
+squareFirstN(0, 10)
